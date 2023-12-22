bec177baf28c14754a83fef63f0adfcdc7de68e2
DigitalPotential <Carlos@digitalpotential.se>
2023-12-22 21:26:17 +0100

    feat: added logo to navbar

---
+++ app/(browse)/_components/Navbar/logo.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1">
+          <Image src="/spooky.svg" alt="GameHub" height="32" width="32" />
+        </div>
+
+        <div>
+          <p>Gamehub</p>
+          <p>Let&apos;s play</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
